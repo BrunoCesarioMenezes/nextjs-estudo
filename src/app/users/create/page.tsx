0594eab2921208791e37a6a785c01aa96c0db018
BrunoCesarioMenezes <brunoagjf@gmail.com>
2025-09-24 14:26:41 -0300

    cria rota api e pagina para criacao de usuarios

---
+++ src/app/users/create/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import useHandleSubmit from "@/hooks/useHandleSubmit";
+
+export default function createUser() {
+  const {msg, handleSubmit} = useHandleSubmit();
+
+  return (
+    <div className="flex min-h-screen min-w-screen justify-center">
+      <div className="flex flex-col gap-2 self-center bg-white text-black rounded-lg p-4 shadow-lg w-fit h-fit">
+        <form onSubmit={handleSubmit} method="post" className="flex flex-col gap-4">
+          <input type="text" name="nome" className="border-2 botder-black" placeholder="Nome" />
+          <input type="text" name="email" className="border-2 botder-black" placeholder="Email" />
+          <button type="submit" className="bg-green-400 p-4">CRIAR</button>
+        </form>
+        {msg && <p className="text-gray-700 font-bold">{msg}</p>}
+      </div>
+    </div>
+  );
+}
